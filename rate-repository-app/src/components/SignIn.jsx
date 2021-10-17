0fb50c3f7e5118be3ad04f668b401d1cccd662e1
Veeeeti <59977109+Veeeeti@users.noreply.github.com>
2021-10-17 14:49:59 +0300

    osa10 teht 10.8

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
-
+import { Pressable, TextInput, View, StyleSheet} from 'react-native';
+import {Formik, useField } from 'formik';
 import Text from './Text';
+import theme from '../theme';
+
+const initialValues = {
+    username: '',
+    password: ''
+}
+
+const styles = StyleSheet.create({
+    loginContainer: {
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    textField: {
+        borderStyle: 'solid',
+        borderColor: theme.colors.textSecondary,
+        borderRadius: 3,
+        padding: 10,
+        margin: 10,
+        paddingHorizontal: 100
+    },
+    loginButton: {
+        backgroundColor: theme.colors.primary,
+        borderRadius: 3,
+        padding: 10,
+        paddingHorizontal: 100
+    }
+});
+
+
+const SignInForm = ({ onSubmit }) => {
+    const [nameField, nameMeta, nameHelpers] = useField('username');
+    const [passwordField, passwordMeta, passwordHelpers] = useField('password');
+    return (
+        <View style={styles.loginContainer}>
+            <TextInput 
+            placeholder="Username"
+            value={nameField.value}
+            onChangeText={text => nameHelpers.setValue(text)}
+            style={styles.textField}
+            />
+            <TextInput 
+            placeholder="Password"
+            value={passwordField.value}
+            onChangeText={text => passwordHelpers.setValue(text)}
+            secureTextEntry={true}
+            style={styles.textField}
+            />
+            <Pressable style={styles.loginButton} onPress={onSubmit}>
+                <Text>Login</Text>
+            </Pressable>
+        </View>
+    );
+}
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+    const onSubmit = (values) => {
+        console.log('submitted',values);
+    }
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {({ handleSubmit}) => <SignInForm onSubmit={handleSubmit}/>}
+        </Formik>
+    );
+        
 };
 
 export default SignIn;
