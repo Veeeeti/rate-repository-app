fbe6f6888f425078cdc2cc7f450b6d916f7651ef
Veeeeti <59977109+Veeeeti@users.noreply.github.com>
2021-10-18 21:06:23 +0300

    osa10 teht 10.12

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -4,31 +4,11 @@ import { GET_REPOSITORIES } from '../graphql/queries';
 
 
 const useRepositories = () => {
-  // const [repositories, setRepositories] = useState();
-  // const [loading, setLoading] = useState(false);
-
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     // Other options
   });
-  // const fetchRepositories = async () => {
-  //   setLoading(true);
-
-  //   // Replace the IP address part with your own IP address!
-  //   // const response = await fetch('http://172.20.10.2:5000/api/repositories');
-  //   // const json = await response.json();
-
-  //   console.log('Fetched data:',data)
-
-  //   setLoading(false);
-  //   setRepositories(data);
-  // };
-
-  // useEffect(() => {
-  //   fetchRepositories();
-  // }, []);
-
-  // return { repositories, loading, refetch: fetchRepositories };
+  
   return {data, error, loading};
 };
 
--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://172.20.10.2:5000/graphql',
+  // uri: 'http://172.20.10.2:5000/graphql',
+  uri: Constants.manifest.extra.uri,
 });
 
 const createApolloClient = () => {
